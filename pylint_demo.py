54f9478e5c6b2822497cd7789538fef4b2a7ac45
Va7er <vy2042@nyu.edu>
2025-03-03 15:22:59 +0400

    Achieved 10/10 on pylint

--- pylint_demo.py
+++ pylint_demo.py
@@ -1,12 +1,24 @@
-def sum(a, b):
+'''
+
+This file contains 2 functions
+
+'''
+
+def summing(a, b):
+    '''
+    Add two numbers and return the result
+    '''
     return a + b
 
 
 def hello(name):
+    '''
+    Print the parameter as a greeting message
+    '''
     print(f"Hello, {name}!")
 
-result = sum (3, 5)
+RESULT = summing (3, 5)
 
-print(result)
+print(RESULT)
 hello("Alice")
 hello("Vahagn")
